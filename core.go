f4d63b33485749f2cc51eb21ceca6fed739c5ded
peter <904801074@qq.com>
2025-07-30 16:54:10 +0800

    优化

--- core.go
+++ core.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/target"
 	"github.com/chromedp/chromedp"
+	"log"
+	"strings"
 	"time"
 )
 
@@ -30,7 +33,7 @@ func (c *ChromedpTool) Debug() *ChromedpTool {
 	return c
 }
 
-func (c *ChromedpTool) initChrome() (context.Context, context.CancelFunc) {
+func (c *ChromedpTool) InitChrome() (context.Context, context.CancelFunc) {
 
 	var opts []chromedp.ExecAllocatorOption
 
@@ -66,6 +69,8 @@ func (c *ChromedpTool) initChrome() (context.Context, context.CancelFunc) {
 }
 
 func (c *ChromedpTool) SetCookie(name, value, domain, path string, httpOnly, secure bool) chromedp.Action {
+	name = strings.TrimSpace(name)
+	value = strings.TrimSpace(value)
 	return chromedp.ActionFunc(func(ctx context.Context) error {
 		expr := cdp.TimeSinceEpoch(time.Now().Add(180 * 24 * time.Hour))
 		err := network.SetCookie(name, value).
@@ -91,4 +96,20 @@ func (c *ChromedpTool) WaitElementExist(ctx context.Context, selector string, ti
 	)
 
 	return err == nil
+}
+
+func (c *ChromedpTool) NewPage(ctx context.Context, callback func(targetID target.ID)) {
+
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		if e, ok := ev.(*target.EventTargetCreated); ok {
+			// 过滤掉非页面类型的新 Target
+			if e.TargetInfo.Type == "page" {
+				if c.debug {
+					log.Println("新标签页打开，ID为" + e.TargetInfo.TargetID)
+				}
+				callback(e.TargetInfo.TargetID)
+			}
+		}
+	})
+
 }
